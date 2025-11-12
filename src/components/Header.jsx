d412ee4d0174e75d67e59798f422c4f35cb50e53
Matteo Granchelli <yozza@Mac.station>
2025-11-12 22:06:36 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./css/Header.css"
+export default function Header(){
+    return(
+        <>
+            <header className="mainHeader">
+                <h1>Welcome to my site!</h1>
+                <i className="volumeOn"></i>
+            </header>
+        </>
+    )
+}
