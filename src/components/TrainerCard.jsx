dd881deeb10fb0ae8544855ff8171a0c6f0091af
Matteo Granchelli <yozza@Mac.station>
2025-11-23 19:25:28 +0100

    Started to create Trainer Card

--- src/components/TrainerCard.jsx
+++ src/components/TrainerCard.jsx
@@ -1,9 +1,17 @@
 import "./css/TrainerCard.css"
-
+import TrainerInfo from "./trainerInfo"
 export default function TrainerCard() {
     return (
         <div className="trainercard">
+            <header className="trainerCardHeader">
+                <h2>Trainer Card</h2>
+            </header>
+            <div className="trainerCardInfoContainer">
+                <TrainerInfo />
+                <div className="trainerIcons">
 
+                </div>
+            </div>
         </div>
 
     )
---
+++ src/components/TrainerInfo.jsx
@@ -0,0 +1,26 @@
+export default function TrainerInfo() {
+    return (
+        <ul className="trainerInfo">
+            <li>
+                <p>Name:</p>
+                <p>Matteo</p>
+            </li>
+            <li>
+                <p>Surname:</p>
+                <p>Granchelli</p>
+            </li>
+            <li>
+                <p>Job:</p>
+                <p>Front End Web Developer</p>
+            </li>
+            <li>
+                <p>Based:</p>
+                <p>Pescara</p>
+            </li>
+            <li>
+                <p>Time:</p>
+                <p>1 year</p>
+            </li>
+        </ul>
+    )
+}
