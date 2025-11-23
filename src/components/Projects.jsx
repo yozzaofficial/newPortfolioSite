aade31ac165d04ea303181e2d76f95c949a23910
Matteo Granchelli <yozza@Mac.station>
2025-11-23 22:10:17 +0100

    Continued to create the squad

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,14 @@ import project from "./../project.js"
 
 export default function Projects() {
     let divElement = project.map(e => {
-        return <div className="projects">{e.name}</div>
+        return <div className="projects">
+            <header className="projectsHeader">{e.name}</header>
+            <div className="hp"></div>
+            <div className="projectDetails">
+                <p>{e.level}</p>
+                <p>{e.hp}/{e.hp}</p>
+            </div>
+        </div>
     })
     return (
         <>
--- src/project.js
+++ src/project.js
@@ -3,7 +3,9 @@ const project = [{
     name: "Matteo Advice",
     description: "Matteo Advice is a small website created to recommend movies and TV series. The site allows users to explore my favorite movies and series, see what I am currently watching, and manage their own preferences.",
     code: "React",
-    link: ""
+    link: "",
+    level: 75,
+    hp: 240,
 },
 {
     id: 2,
@@ -11,6 +13,8 @@ const project = [{
     description: "Assembly Endgame is a small browser game built with React, inspired by the classic hangman — but with a programming twist.Guess the word, or witness the death of iconic programming languages.",
     code: "React",
     link: "",
+    level: 60,
+    hp: 196,
 },
 {
     id: 3,
@@ -18,6 +22,8 @@ const project = [{
     description: "Tenzies is a small game built with React and Vite. The goal is to get all the dice to show the same number. You can hold dice you want to keep and roll the others until you win!",
     code: "React",
     link: "",
+    level: 60,
+    hp: 187,
 }
 ]
 
