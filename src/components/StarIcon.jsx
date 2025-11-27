f5d60b9603ccc7ac836bd89d58cf965eb17d5067
Matteo Granchelli <yozza@Mac.station>
2025-11-27 10:18:48 +0100

    Trainer card updated

---
+++ src/components/StarIcon.jsx
@@ -0,0 +1,10 @@
+export default function StarIcon() {
+
+    return <>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+    </>
+}
--- src/components/TrainerCard.jsx
+++ src/components/TrainerCard.jsx
@@ -1,5 +1,8 @@
+import React from "react"
 import "./css/TrainerCard.css"
+import StarIcon from "./StarIcon"
 import TrainerInfo from "./trainerInfo"
+
 export default function TrainerCard() {
     return (
         <div className="trainercard">
@@ -11,9 +14,15 @@ export default function TrainerCard() {
             <div className="trainerCardInfoContainer">
                 <TrainerInfo />
                 <div className="trainerIcons">
-                    <div className="starContainer"></div>
-                    <div className="iconTraienr"></div>
-                    <div className="linksContainer"></div>
+                    <div className="starContainer">
+                        <StarIcon />
+                    </div>
+                    <div className="iconTrainer"></div>
+                    <div className="linksContainer">
+                        <a ><div className="gitHub-icon"></div></a>
+                        <a><div className="linkedin-icon"></div></a>
+                        <a><div className="mail-icon"></div></a>
+                    </div>
                 </div>
             </div>
         </div>
